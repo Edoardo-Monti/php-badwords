365256c1f4ac39a51821274a15c1bd217a3c5986
Your Namede <edoardo.monti.bo@gmail.com>
2023-05-18 17:04:03 +0200

    creazione form

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php start</title>
+</head>
+<body>
+    <form action="arrivo.php" method="get">
+        <div>
+            <label for="">Nome</label>
+            <input type="text" placeholder="Inserisci il tuo nome">
+
+            <label for="">Cognome</label>
+            <input type="text" placeholder="Inserisci il tuo cognome">
+
+            <button type="submit">Send Form</button>
+        </div>
+    </form>
+</body>
+</html>
